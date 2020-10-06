94f83341897e10fd846a89dd51f4fa3eb779d189
Zhang, Chen <zhangc@ornl.gov>
2020-10-06 12:04:07 -0400

    add place holder module, testfile, scripts

---
+++ scripts/mcp_detector_correction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+This is a processing script for MCP detector at SNAP beamline (Imaging),
+which corrects the detector count rate (detector efficiency).
+"""
+
+
+if __name__ == "__main__":
+    pass
---
+++ tests/test_detector_correction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+Unit testing for detector correction module from package
+NetronImaging
+"""
+import pytest
+
+if __name__ == '__main__':
+  pytest.main([__file__])
