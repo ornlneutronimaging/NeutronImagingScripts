9fea627e57ad160bc45a65048fbcf436ab402f35
Zhang, Chen <zhangc@ornl.gov>
2020-10-06 16:46:26 -0400

    add helper function determine if running in notebook

---
+++ neutronimaging/util.py
@@ -0,0 +1,20 @@
+#!/usr/env/bash
+
+"""
+Provide various useful helper functions that
+handles system level tasks.
+"""
+
+
+def in_jupyter():
+    try:
+        from IPython import get_ipython
+        kernel_name = get_ipython().__class__.__name__
+        state = True if "ZMQ" in kernel_name else False
+    except NameError:
+        state = False
+    return state    
+
+
+if __name__ == "__main__":
+    pass
