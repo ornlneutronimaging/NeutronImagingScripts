3a81928796b37d53b8192bb140f3ef4e4cec2a92
Zhang, Chen <zhangc@ornl.gov>
2020-10-07 09:38:59 -0400

    add infile testing

--- neutronimaging/detector_correction.py
+++ neutronimaging/detector_correction.py
@@ -19,4 +19,12 @@ def read_shutter_count(filename: str) -> pd.DataFrame:
     return _df[_df['shutter_counts'] > 0]
 
 if __name__ == "__main__":
+    import os
+    _file_root = os.path.dirname(os.path.abspath(__file__))
+    test_data_dir = os.path.join(_file_root, "../data")
+    #
+    shutter_counts_file = os.path.join(test_data_dir,"OB_1_005_ShutterCount.txt")
+    df_shutter_count = read_shutter_count(shutter_counts_file)
+    print(df_shutter_count)
+    #
     pass
