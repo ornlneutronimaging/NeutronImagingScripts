2a57b8326f3bd9ed7fae0bc7de5274f12fbe87ea
Zhang, Chen <zhangc@ornl.gov>
2020-10-07 09:39:10 -0400

    bug fix

--- neutronimaging/detector_correction.py
+++ neutronimaging/detector_correction.py
@@ -8,12 +8,12 @@ import pandas as pd
 
 
 def read_shutter_count(filename: str) -> pd.DataFrame:
-    """Parse in shutter count data from CSV"""
+    """Parse in shutter count data from csv"""
     # TODO: confirm whether we should stop or skip at
     #       zero count location
     _df =  pd.read_csv(
         filename,
-        setp="\t",
+        sep="\t",
         names=['shutter_index', 'shutter_counts']
         )
     return _df[_df['shutter_counts'] > 0]
