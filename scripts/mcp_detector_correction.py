78ebd9d12a67f88f489e7293a99f8ca6af33daae
Zhang, Chen <zhangc@ornl.gov>
2020-10-07 20:34:24 -0400

    reformat with black

--- scripts/mcp_detector_correction.py
+++ scripts/mcp_detector_correction.py
@@ -18,13 +18,18 @@ Options:
 import glob
 from docopt import docopt
 from pathlib import Path
-from neutronimaging.detector_correction import correct_images, load_images, read_shutter_count
-from neutronimaging.detector_correction import read_shutter_time
-from neutronimaging.detector_correction import read_spectra
-from neutronimaging.detector_correction import merge_meta_data
+from neutronimaging.detector_correction import (
+    correct_images,
+    load_images,
+    read_shutter_count,
+    read_shutter_time,
+    read_spectra,
+    merge_meta_data,
+)
+
 
 if __name__ == "__main__":
-    args = docopt(__doc__, help=True, version='MCP Detector Correction 1.0')
+    args = docopt(__doc__, help=True, version="MCP Detector Correction 1.0")
 
     # parsing input
     print("Parsing input")
@@ -32,9 +37,9 @@ if __name__ == "__main__":
     output_dir = args["<output_dir>"]
     skip_first_last_img = args["--skipimg"]
     verbose = args["--verbose"]
-    shutter_count_file = glob.glob(input_dir + '/*_ShutterCount.txt')[0]
-    shutter_time_file = glob.glob(input_dir + '/*_ShutterTimes.txt')[0]
-    spectra_file = glob.glob(input_dir + '/*_Spectra.txt')[0]
+    shutter_count_file = glob.glob(input_dir + "/*_ShutterCount.txt")[0]
+    shutter_time_file = glob.glob(input_dir + "/*_ShutterTimes.txt")[0]
+    spectra_file = glob.glob(input_dir + "/*_Spectra.txt")[0]
 
     # validation
     print("Validating input arguments")
@@ -47,13 +52,13 @@ if __name__ == "__main__":
     # process metadata
     print("Processing metadata")
     df_meta = merge_meta_data(
-        read_shutter_count(shutter_count_file), 
-        read_shutter_time(shutter_time_file), 
+        read_shutter_count(shutter_count_file),
+        read_shutter_time(shutter_time_file),
         read_spectra(spectra_file),
-        )
+    )
     if verbose:
         print(df_meta)
-    
+
     # load images
     print("Loading images into memory")
     o_norm = load_images(input_dir)
@@ -61,15 +66,15 @@ if __name__ == "__main__":
     # perform image correction
     print("Perform correction")
     img_corrected = correct_images(
-        o_norm, 
-        df_meta, 
+        o_norm,
+        df_meta,
         skip_first_and_last=skip_first_last_img,
-        )
+    )
     print("corrected image summary")
     print(f"\tdimension:\t{img_corrected.shape}")
     print(f"\ttype:\t{img_corrected.dtype}")
-    
+
     # export results
     print(f"Writing data to {output_dir}")
-    o_norm.data['sample']['data'] = img_corrected
-    o_norm.export(folder=output_dir, data_type='sample')
+    o_norm.data["sample"]["data"] = img_corrected
+    o_norm.export(folder=output_dir, data_type="sample")
