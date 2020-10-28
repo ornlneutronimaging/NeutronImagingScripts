71a947951963f6cd2d70d7fa13385692274f42af
Zhang, Chen <zhangc@ornl.gov>
2020-10-28 12:14:49 -0400

    allow adjustment of tolerance in CLI

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -4,13 +4,14 @@
 """Configuration Generator
 
 Usage:
-    generate_config.py  <input_dir> <output_file>
+    generate_config.py  <input_dir> <output_file> [--tolerance=<tor>]
     generate_config.py  (-h | --help)
     generate_config.py  --version
 
 Options:
-    -h --help   print this message
-    --version   print version info
+    -h --help           print this message
+    --version           print version info
+    --tolerance=<tor>   tolerance for slit position during clustering  [defualt: 1]
 """
 
 from docopt import docopt
@@ -24,6 +25,7 @@ if __name__ == "__main__":
     print('Parsing input')
     rootdir = args['<input_dir>']
     output = args['<output_file>']
+    tor = 1.0 if args['--tolerance'] is None else float(args['--tolerance'])
 
     # generate config
-    generate_config_CG1D(rootdir, output=output)
+    generate_config_CG1D(rootdir, output=output, tolerance_aperature=tor)
