3626153256458f1903bf163e8e17a36a83da8805
Zhang, Chen <zhangc@ornl.gov>
2020-10-27 20:00:48 -0400

    reformatted with black

--- neutronimaging/npmath.py
+++ neutronimaging/npmath.py
@@ -11,7 +11,7 @@ from typing import List
 import numpy as np
 
 
-def find_edges_1d(array: np.ndarray, atol: float=1) -> List:
+def find_edges_1d(array: np.ndarray, atol: float = 1) -> List:
     """
     Locate the lower and upper edges of binning using given absoute
     tolerance.
@@ -21,12 +21,16 @@ def find_edges_1d(array: np.ndarray, atol: float=1) -> List:
     >> [(37.5, 38.5), (38.5, 41.0), (41.0, 42.5)]
     """
     if len(array) <= 1:
-        return [(min(array)-atol/2, max(array)+atol/2)]
+        return [(min(array) - atol / 2, max(array) + atol / 2)]
     else:
         array = np.sort(array)
         gaps = array[1:] - array[:-1]
-        lower_edge = [array[0]-atol/2] + list(array[np.where(gaps>atol/2)]+atol/2)
-        upper_edge = list(array[np.where(gaps>atol/2)] + atol/2) + [array[-1]+atol/2]
+        lower_edge = [array[0] - atol / 2] + list(
+            array[np.where(gaps > atol / 2)] + atol / 2
+        )
+        upper_edge = list(array[np.where(gaps > atol / 2)] + atol / 2) + [
+            array[-1] + atol / 2
+        ]
         return zip(lower_edge, upper_edge)
 
 
