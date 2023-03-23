0a21ffbd04d9a1171e6cec265fd6d8dbbd7d7734
Chen Zhang <zhangc@ornl.gov>
2023-03-23 13:14:55 -0400

    adding mars specific sub module

---
+++ neutronimaging/mars/normalization.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Normalization code.
+
+The original implementation is implemented at:
+
+https://github.com/neutronimaging/python_notebooks/blob/next/notebooks/__code/normalization/normalization_with_simplify_selection.py
+"""
+import logging
